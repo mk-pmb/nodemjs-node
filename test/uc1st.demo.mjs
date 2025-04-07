1359fe65a39666aea204a40bef7273b35edb5523
mk-pmb <mk-pmb@users.noreply.github.com>
2025-04-07 06:05:14 +0200

    [test] Add test for local relative file import.

---
+++ test/uc1st.demo.mjs
@@ -0,0 +1,8 @@
+// -*- coding: utf-8, tab-width: 2 -*-
+
+const EX = {
+  dummyInput: 'using dummy input because there were no CLI args'.match(
+    /\S+(?:\s+\S+){0,2}/g),
+};
+
+export default EX;
--- test/uc1st.mjs
+++ test/uc1st.mjs
@@ -3,11 +3,11 @@
 
 import uc1st from 'lodash.upperfirst';
 
+import demo from './uc1st.demo.mjs';
+
+
 async function main(...input) {
-  if (!input.length) {
-    const demo = 'using dummy input because there were no CLI args';
-    return main(...demo.match(/\S+(?:\s+\S+){0,2}/g));
-  }
+  if (!input.length) { return main(...demo.dummyInput); }
   const prs = input.map(async s => uc1st(s));
   const arr = await Promise.all(prs);
   console.log('uc1st:', arr);
